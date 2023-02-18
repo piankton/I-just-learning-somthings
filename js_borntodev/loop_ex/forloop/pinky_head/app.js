6bee39a95da872922df1eb0f15caf7d03c2457f0
piankton <chonlakorn16@gmail.com>
2023-02-18 21:04:14 +0700

    add count time to win

--- js_borntodev/loop_ex/forloop/pinky_head/app.js
+++ js_borntodev/loop_ex/forloop/pinky_head/app.js
@@ -1,21 +1,24 @@
 let round = prompt("how mush to play?");
-
+var total_currect =  0;
 for(var i=0; i<round; i++){
-    var answer = prompt("Head or pinky?");
+    var answer = prompt("หัว หรือ ก้อย");
     let random_coin = randomnumber = Math.random();
     let random_awnser = "";
 
-    if(random_coin = 0){
+    if(random_coin == 0){
         random_awnser = "หัว"
     }else{
         random_awnser = "ก้อย"
     }
 
     if(answer == random_awnser){
-        document.getElementById("game-list").innerHTML+="YOU WIN : "+ answer + "answer : random" +random_awnser + "<br />";
+        document.getElementById("game-list").innerHTML+="YOU WIN : "+ answer + " - answer : random - " +random_awnser + "<br />";
+        total_currect++;
     }else{
-        document.getElementById("game-list").innerHTML+="YOU FOOL : "+ answer + "answer : random" +random_awnser + "<br />";
+        document.getElementById("game-list").innerHTML+="YOU FOOL : "+ answer + " -answer : random - " +random_awnser + "<br />";
     }
-
-    console.log(answer + "<answer : random_answer>" +random_awnser)
+    console.log(answer + "<-- answer : random_answer -->" +random_awnser)
+    console.log(total_currect) 
 }
+document.getElementById("wincount").innerHTML = "You Win: "+ total_currect + " Times"   
+

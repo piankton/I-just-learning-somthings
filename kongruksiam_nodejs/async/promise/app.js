e4c3ff711d2b91fee0cdd118087b264257bcfd1a
piankton <chonlakorn16@gmail.com>
2023-03-11 14:58:36 +0700

    add how to use then to slove callbackhell

--- kongruksiam_nodejs/async/promise/app.js
+++ kongruksiam_nodejs/async/promise/app.js
@@ -54,8 +54,42 @@
 // });
 //---------------------------
 //* -[ ทดลองใช้ promise + metod ]-
+// const connect = true;
+// const url1 = "mean.dev/tele"
+
+// function downloading(url){
+//     return new Promise(function(reslove,reject){
+//         console.log("downloading...")
+//         setTimeout(()=>{
+//             if(connect){
+//                 reslove(`download ${url} done`)
+//                     success
+//             }else{
+//                 reject("error");
+//                     error
+//             }
+//         },1500);
+//     })
+// }
+// ส่งสถานะสำเร็จไปที่ .then
+// ถ้าerrorไป .catch
+// finally ทำงานสุดท้ายเสมอ
+// downloading(url1).then(result =>{
+//     console.log(result)
+// }).catch(err=>{
+//     console.log(err)
+// }).finally(()=>{
+//     console.log("จบการทำงาน")
+// });
+//---------------------------
+//* -[ ทดลองใช้ promise แก้ไข callbackhell ]-
 const connect = true;
-const url1 = "mean.dev/tele"
+const url1 = "mean.dev/tele1"
+const url2 = "mean.dev/tele2"
+const url3 = "mean.dev/tele3"
+const url4 = "mean.dev/tele4"
+const url5 = "mean.dev/tele5"
+
 
 function downloading(url){
     return new Promise(function(reslove,reject){
@@ -71,13 +105,25 @@ function downloading(url){
         },1500);
     })
 }
-//ส่งสถานะสำเร็จไปที่ .then
-//ถ้าerrorไป .catch
-//finally ทำงานสุดท้ายเสมอ
-downloading(url1).then(result =>{
+//! ไม่ควรเขียนแบบนี้
+// downloading(url1).then(function(result){
+//     console.log(result);
+//     downloading(url2).then(function(result){
+//         console.log(result)
+//         downloading(url3).then(function(result){
+//             console.log(result)
+//         });
+//     });
+// });
+
+//? ควรใช้แบบนี้
+
+downloading(url1).then(function(result){
     console.log(result)
-}).catch(err=>{
-    console.log(err)
-}).finally(()=>{
-    console.log("จบการทำงาน")
-});
+    return downloading(url2)
+}).then(function(result){
+    console.log(result)
+    return downloading(url2)
+}).then(function(result){
+    console.log(result)
+})
